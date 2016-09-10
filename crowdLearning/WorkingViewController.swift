5cf25933c124ebae417d4f581da1890ce2032621
kevinfang2 <kevkev0123@gmail.com>
2016-09-10 02:52:48 -0400

    same

---
+++ crowdLearning/WorkingViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WorkingViewController.swift
+//  crowdLearning
+//
+//  Created by Kevin Fang on 9/10/16.
+//  Copyright © 2016 myh1000. All rights reserved.
+//
+
+import UIKit
+
+class WorkingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ crowdLearning/landingViewController.swift
@@ -0,0 +1,41 @@
+//
+//  landingViewController.swift
+//  crowdLearning
+//
+//  Created by Kevin Fang on 9/10/16.
+//  Copyright © 2016 myh1000. All rights reserved.
+//
+
+import UIKit
+
+class landingViewController: UIViewController {
+
+    @IBOutlet weak var joinButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        joinButton.layer.cornerRadius = 5;
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func prepareForUnwind(segue: UIStoryboardSegue) {
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
