127baedabe75c8abd2167dd6a036441c2d23f119
myh1000 <myh1000@gmail.com>
2016-09-09 21:22:40 -0400

    init code

---
+++ crowdLearning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  crowdLearning
+//
+//  Created by family on 9/9/16.
+//  Copyright © 2016 myh1000. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
